72567e1211758f94d470121ad48d48842c19fdec
furste <furste@msoe.edu>
2023-11-21 15:33:26 -0600

    item added to context menu

---
+++ background.js
@@ -0,0 +1,22 @@
+function onCreated() {
+    if (browser.runtime.lastError) {
+        alert(`Error: ${browser.runtime.lastError}`);
+    } else {
+        alert("Item created successfully");
+    }
+}
+
+browser.menus.create({
+    id: "summarize",
+    title: "Summarize Article",
+    },
+    onCreated,
+);
+
+browser.menus.onClicked.addListener((info, tab) => {
+    switch (info.menuItemId) {
+        case "summarize":
+            alert("summarize selected");
+            break;
+    }
+});
