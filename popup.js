bef384fcf34e2057ab8e405bb4a49a9d00ece7b3
kraftzenka <kraftzenka@msoe.edu>
2023-11-28 16:15:06 -0600

    user preferences that save + load, always visible help, and no profanity!

--- popup.js
+++ popup.js
@@ -1,19 +1,58 @@
 window.onload = () => {
 
-    const helpButton = document.getElementById("helpButton");
-    const helpDiv = document.getElementById("helpDiv");
+    //const helpButton = document.getElementById("helpButton");
+    //const helpDiv = document.getElementById("helpDiv");
 
-    helpButton.onclick = help;
+    const gptKeyInput = document.getElementById("gptKey");
+    const responseLengthPreferenceInput = document.getElementById("responseLengthPreference");
+    const responseLengthPreferenceDisplay = document.getElementById("responseLengthPreferenceDisplay")
 
-    function help() {
-        helpButton.onclick = close;
-        helpButton.textContent = "Close Help";
-        helpDiv.hidden = false;
+    gptKeyInput.onkeydown = (event) => {
+        if (event.key === "Enter") {
+            gptKeyInput.blur();
+        }
     }
 
-    function close() {
-        helpButton.onclick = help;
-        helpButton.textContent = "?";
-        helpDiv.hidden = true;
+    //adjust response length
+    function setResponseLengthPreference(newValue) {
+        browser.storage.local.set({
+                responseLength: newValue
+        });
     }
+
+    function setGPTKeyPreference(newValue) {
+        browser.storage.local.set({
+            gptKey: newValue
+        });
+    }
+
+    function updateResponseLengthDisplay() {
+        responseLengthPreferenceDisplay.textContent = responseLengthPreferenceInput.value.toString();
+    }
+
+    responseLengthPreferenceInput.oninput = () => {
+        updateResponseLengthDisplay();
+    }
+
+    responseLengthPreferenceInput.onchange = () => {
+        setResponseLengthPreference(responseLengthPreferenceInput.value);
+    }
+
+    gptKeyInput.onchange = () => {
+        setGPTKeyPreference(gptKeyInput.value);
+    }
+
+    browser.storage.local.get("responseLength").then((value) => {
+        if (typeof value !== 'undefined' && typeof value.responseLength !== 'undefined') {
+            responseLengthPreferenceInput.value = value.responseLength;
+            updateResponseLengthDisplay();
+        }
+    });
+
+    browser.storage.local.get("gptKey").then((value) => {
+        if (typeof value !== 'undefined' && typeof value.gptKey !== 'undefined'){
+            gptKeyInput.value = value.gptKey;
+        }
+    })
+
 }
